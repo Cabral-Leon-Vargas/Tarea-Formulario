7b100c88dd2737b0b52d6b807ce8662292baf05c
asalmero <71348959+asalmero@users.noreply.github.com>
2020-09-23 17:27:57 -0500

    New VisualStudio Project

--- Program.cs
+++ Program.cs
@@ -1,32 +1,42 @@
 ﻿using System;
-using System.Net;
 
 namespace AplicacionPrueba
 {
     class Program
     {
-
         static void Main(string[] args)
 
-        { 
-             int number, i, j, count = 1; // Variables Principales
-            Console.Write("Enter a Row: "); //Row value
-            number = int.Parse(Console.ReadLine());
+        {
+            int number, i, j, count = 1;
+            Console.Write("Rows :");
+            number = int.Parse(Console.ReadLine()); // Convertidor de Tipo de dato
             count = number - 1;
-            
-            for (j = 1; j <= number; j++) // Ciclo Principal
+            for (j = 1; j <= number; j++)
             {
-                for (i = 1; i <= count; i++) // For Anidado
-                    Console.Write(" "); //Espacios
+                for (i = 1; i <= count; i++)
+                    Console.Write(" ");
                 count--;
-
-                for (i = 1; i <= 2 * j - 1; i++) // Segundo Ciclo Anidado
-                    Console.Write("*"); //Impresion de mi simbolo
+                //Edita la linea de abajo por favor.
+                for (i = 1; i <= 2 * j - 1; i++)
+                    Console.Write("*");
                 Console.WriteLine();
             }
-        
-            Console.WriteLine();
-             
+            // Console.ReadLine();
+            count = 1;
+            for (j =1; j<= number - 1; j++)
+            {
+                for (i = 1; i <= count; i++)
+                    Console.Write(" ");
+                count++;
+                for (i = 1; i <= 2 * (number - j) - 1; i++)
+                    Console.Write("*");
+                Console.WriteLine();
+                      
+            }
+
+             Console.ReadLine();
+
+
         }
     }
    
