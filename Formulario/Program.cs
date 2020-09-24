42fb664e74fcf7b4531fcf8b55b980594fcb8503
erendira-cabral <71351745+erendira-cabral@users.noreply.github.com>
2020-09-24 16:57:36 -0500

    formulario

--- Formulario/Program.cs
+++ Formulario/Program.cs
@@ -6,7 +6,7 @@ namespace Formulario
     {
         static void Main(string[] args)
         {
-            String nom, numtel;
+            String nom, numtel,correo,ocup;
             int edad;
             Console.WriteLine("Escribe tu nombre completo: ");
             nom = Console.ReadLine();
@@ -14,18 +14,21 @@ namespace Formulario
             edad = int.Parse(Console.ReadLine());
             Console.WriteLine("\nEscribe tu numero de telefono");
             numtel = Console.ReadLine();
+            Console.WriteLine("\nEscribe tu correo electronico");
+            correo = Console.ReadLine();
+            Console.WriteLine("\n¿Cuál es tu ocupación? ");
+            ocup = Console.ReadLine();
 
-            
             if (edad < 18) 
             {
-                Console.WriteLine("El registro solo es para mayores de edad");
+                Console.WriteLine("Lo sentimos, El registro solo es permitido para mayores de edad");
                 
 
 
             }
             else
             {
-               
+                Console.Write("El registro fue existoso\n\n");
                 
             }
         }
