45d6e789f7e717b5482df46d4213a6a9fd5858bc
MarcoJeanLeon <marco_jleont.ibm@upci.edu.mx>
2020-09-24 16:03:52 -0500

    formulario

--- Formulario/Program.cs
+++ Formulario/Program.cs
@@ -6,18 +6,15 @@ namespace Formulario
     {
         static void Main(string[] args)
         {
-            String nom, numtel, correo, ocup;
-            int edad, rep;
+            String nom, numtel;
+            int edad;
             Console.WriteLine("Escribe tu nombre completo: ");
             nom = Console.ReadLine();
             Console.WriteLine("\nEscribe tu edad: ");
             edad = int.Parse(Console.ReadLine());
             Console.WriteLine("\nEscribe tu numero de telefono");
             numtel = Console.ReadLine();
-            Console.WriteLine("\nEscribe tu correo electronico");
-            correo = Console.ReadLine();
-            Console.WriteLine("\n¿Cuál es tu ocupación?");
-            ocup = Console.ReadLine();
+
             
             if (edad < 18) 
             {
@@ -28,13 +25,7 @@ namespace Formulario
             }
             else
             {
-                Console.WriteLine("El registro fue exitoso\n\n");
-
-                Console.WriteLine("\nTu nombre es: {0}", nom);
-                Console.WriteLine("\nTu edad es: {0}", edad);
-                Console.WriteLine("\nTu numero de telefono es: {0}", numtel );
-                Console.WriteLine("\nTu correoelectronico es: {0}", correo);
-                Console.WriteLine("\nTu ocupación es: {0}", ocup);
+               
                 
             }
         }
