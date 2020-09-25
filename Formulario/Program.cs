0889c30cc52546e90ab021f7bcd416aa9df04009
Victoriavargasespinosa <Victoriavargasespinosa@users.noreply.github.com>
2020-09-25 11:53:33 -0500

    Formulario final

--- Formulario/Program.cs
+++ Formulario/Program.cs
@@ -12,16 +12,16 @@ namespace Formulario
             nom = Console.ReadLine();
             Console.WriteLine("\nEscribe tu edad: ");
             edad = int.Parse(Console.ReadLine());
-            Console.WriteLine("\nEscribe tu numero de telefono");
+            Console.WriteLine("\nEscribe tu numero de teléfono");
             numtel = Console.ReadLine();
-            Console.WriteLine("\nEscribe tu correo electronico");
+            Console.WriteLine("\nEscribe tu correo electrónico");
             correo = Console.ReadLine();
             Console.WriteLine("\n¿Cuál es tu ocupación? ");
             ocup = Console.ReadLine();
 
             if (edad < 18) 
             {
-                Console.WriteLine("Lo sentimos, El registro solo es permitido para mayores de edad");
+                Console.WriteLine("Lo sentimos, el registro solo es permitido para mayores de edad");
                 
 
 
@@ -29,7 +29,13 @@ namespace Formulario
             else
             {
                 Console.Write("El registro fue existoso\n\n");
-                
+
+                Console.WriteLine("\nTu nombre es: {0}", nom);
+                Console.WriteLine("\nTu edad es: {0}", edad);
+                Console.WriteLine("\nTu número de teléfono es: {0}", numtel);
+                Console.WriteLine("\nTu correo electrónico es: {0}", correo);
+                Console.WriteLine("\nTu ocupación es: {0, ocup);");
+           
             }
         }
     }
